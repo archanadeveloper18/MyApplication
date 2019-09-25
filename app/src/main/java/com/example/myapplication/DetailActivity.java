4c2be7682ce74737c5b142892d10554832957c96
Archana Jha <archanadeveloper18@gmail.com>
2019-09-24 19:50:14 -0600

    Update DetailActivity.java

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -5,10 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+    
+    TextView productName;
+    ImageView productImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        
+        //set data
+        productName = findViewById(R.id.textcash);
+        productImage = findViewById(R.id.imageView2);
+
+        //get Intent
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("name");
+        String image = intent.getStringExtra("image");
+
+
+        //Set data
+        productName.setText(name);
+
+        Glide.with(this)
+                .load(image)
+                .into(productImage);
     }
 }
