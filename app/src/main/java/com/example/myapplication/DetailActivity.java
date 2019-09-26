30d90cb40cce878f5da976a8e122e672b7ed4e36
Archana Jha <archanadeveloper18@gmail.com>
2019-09-25 22:28:40 -0600

    Update DetailActivity.java

--- app/src/main/java/com/example/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/myapplication/DetailActivity.java
@@ -8,25 +8,39 @@ public class DetailActivity extends AppCompatActivity {
     
     TextView productName;
     ImageView productImage;
+    TextView productcash;
+    TextView productdescription;
+    TextView productterms;
+    TextView productinstore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         
-        //set data
+        //set data to get by their id.
         productName = findViewById(R.id.textcash);
         productImage = findViewById(R.id.imageView2);
+        productcash = findViewById(R.id.textname);
+        productdescription = findViewById(R.id.textdescription);
+        productterms = findViewById(R.id.textterms);
+        productinstore = findViewById(R.id.instore);
 
         //get Intent
         Intent intent = getIntent();
 
         String name = intent.getStringExtra("name");
         String image = intent.getStringExtra("image");
+        String value = intent.getStringExtra("value");
+        String description = intent.getStringExtra("description");
+        String terms = intent.getStringExtra("terms");
 
 
-        //Set data
+        //Set data to display in detail Screen.
         productName.setText(name);
+        productcash.setText(value);
+        productdescription.setText(description);
+        productterms.setText(terms);
 
         Glide.with(this)
                 .load(image)
