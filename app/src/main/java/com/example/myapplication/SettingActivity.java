1a92b4ed7be7ca87d671578a938c5b54c6e72d7c
Archana Jha <archanadeveloper18@gmail.com>
2019-09-25 22:33:50 -0600

    Update SettingActivity.java

--- app/src/main/java/com/example/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -12,8 +12,23 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        TextView  textView = findViewById(R.id.textSetting);
-        TextView textView1 = findViewById(R.id.textdefault);
+         ImageView imageViewGrocery = findViewById(R.id.grocery);
+         TextView textViewGrocery = findViewById(R.id.grocerytext);
+
+        ImageView imageViewPhone = findViewById(R.id.phone);
+        TextView textViewPhone = findViewById(R.id.phonetext);
+
+        ImageView imageViewHome= findViewById(R.id.home);
+        TextView textViewHome = findViewById(R.id.hometext);
+
+        ImageView imageViewTravel = findViewById(R.id.travel);
+        TextView textViewTravel= findViewById(R.id.traveltext);
+
+        ImageView imageViewPharmacy = findViewById(R.id.pharmacy);
+        TextView textViewPharmacy = findViewById(R.id.phonetext);
+
+        ImageView imageViewRestaurant = findViewById(R.id.restaurant);
+        TextView textViewRestaurant = findViewById(R.id.restauranttext);
 
     }
 }
