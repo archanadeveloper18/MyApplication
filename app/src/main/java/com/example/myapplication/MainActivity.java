66624d6229937f5fb233ad6d6b4df78552d1a35a
Archana Jha <archanadeveloper18@gmail.com>
2019-09-25 22:32:34 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,14 +26,14 @@ public class MainActivity extends AppCompatActivity {
     
     public static List<items> itemsList;
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         grideView = findViewById(R.id.gridView);
-        
-         itemsList = new ArrayList<>();
+
+        itemsList = new ArrayList<>();
 
         //make network call
         Call<List<items>> call = APIclient.apIinterface().getitems();
@@ -42,31 +42,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<items>> call, Response<List<items>> response) {
                 if(response.isSuccessful()){
-                    
+
                     itemsList = response.body();
 
                     customAdapter = new CustomAdapter(response.body(),MainActivity.this);
 
                     grideView.setAdapter(customAdapter);
-                    
+
+                    //onclicklistiner is used in grideview when user click on the items of grideview it will show th detail screen.
                     grideView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                             //Intent to go to detail Activity
-
                             Intent intent = new Intent();
-
-                         //   intent.putExtra("name", itemsList.get(position).getName());
-                         //   intent.putExtra("image",itemsList.get(position).getUrl());
-                         // intent.putExtra("value",itemsList.get(position).getCurrentValue());
 
                             startActivity(new Intent(getApplicationContext(), DetailActivity.class)
                                     .putExtra("name", itemsList.get(position).getName())
-                                    .putExtra("image",itemsList.get(position).getUrl()));
+                                    .putExtra("image",itemsList.get(position).getUrl())
+                                    .putExtra("value",itemsList.get(position).getCurrentValue())
+                                    .putExtra("description",itemsList.get(position).getDescription())
+                                    .putExtra("terms",itemsList.get(position).getTerms()));
                         }
                     });
-                    
-                     Log.d("body", response.body().toString());
+
+
+                    Log.d("body", response.body().toString());
                     Log.d("name", response.body().get(0).name);
                     Log.d("url", response.body().get(0).url);
                     Log.d("value", response.body().get(0).current_value.substring(1,5).trim());
@@ -74,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
                         System.out.print(response.body().get(i).getCurrentValue().substring(1,5));
 
                     }
-
 
                 }else{
                     Toast.makeText(getApplicationContext()," An error occured ", Toast.LENGTH_LONG).show();
@@ -89,18 +89,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
-     @Override
+
+    //This methods is call when the user click the menu at the toolbar(top).
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-
+    //This methods is call when the user click the sub list of menu at the toolbar(top) it will go to browsebyctegory Screen..
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_search:
+            case R.id.category:
                 Intent intent = new Intent(this, SettingActivity.class);
                 startActivity(intent);
                 return true;
